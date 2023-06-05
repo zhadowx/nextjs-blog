de88a2eb901ab0de517a4fbc74211ffed5dadd70
Nelson Lora <nlorax@gmail.com>
2023-06-04 22:40:46 -0400

    Edit home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout, { siteTitle } from "../components/layout";
 
 import Head from "next/head";
+import Link from "next/link";
 import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
@@ -20,11 +21,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm a nice dude</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>This is a description of me.</p>
+        <Link href="/posts/first-post">visit post</Link>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
